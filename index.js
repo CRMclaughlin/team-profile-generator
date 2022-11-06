dc177f48988d3121802622b1f410c6fdfc7f3830
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-06 11:37:22 -0600

    added more folders/files, started prompt for application

---
+++ index.js
@@ -0,0 +1,67 @@
+// import npm packages
+
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+// Class import
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// Import HTML template
+const templateHTML = require('./public/index.html');
+
+// Empty array for team members
+const teamMembers = [];
+
+
+// Get manager data inputs to start application
+
+function addManager() {
+    inquirer.createPromptModule([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the team manager?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID of the team manager?',
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the team manager?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email address of the team manager?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the office number of the team manager?',
+        },
+    ])
+    .then((val) => {
+        const manager = new Manager(val.name, val.id, val.email, val.officeNumber)
+        console.table(manager);
+        teamMembers.push(manager);
+        addTeamMember();
+    })
+}
+
+
+
+
+
+
+
+// start of the application prompt for team managers name, employee id, email, and office number
+// then menu with option ot add engineer, intern, or to finish building my team
+// then engineer's name, id, email, and githu usersname, and taken back to menu
+// then prompted to enter the interns name, id, email, and school, takend back to menut
+// when I decide to finish building my team, then I exit the application, an html is generated
