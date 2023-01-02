6ed0335d20ba71f7d9f55758712dc2c995695c44
CRMclaughlin <chrisraymusik@gmail.com>
2023-01-02 15:05:18 -0600

    Changed import from to require for all lib files to work with jest

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,4 +1,4 @@
-import Employee from '../lib/Employee';
+const Employee= require('../lib/Employee') ;
 
 const name = 'Gizmo';
 const id = 666292;
@@ -6,21 +6,25 @@ const email = 'legiz@giz.omc'
 
 const employee = new Employee(name, id, email)
 
-it('Should get an employee name', () => {
-    expect(employee.getName()).toEqual(name)
+describe('Creates an employee object', () => {
+
+    it('Should get an employee name', () => {
+        expect(employee.getName()).toEqual(name)
+    })
+
+    it('Should get an employee ID', () => {
+        expect(employee.id).toEqual(id)
+    })
+
+    it('Should get an employee email', () => {
+        expect(employee.email).toEqual(email)
+    })
+
+    it('Should have the correct role', () => {
+        expect(employee.getRole()).toEqual("Employee")
+    })
 })
 
-it('Should get an employee ID', () => {
-    expect(employee.id).toEqual(id)
-})
-
-it('Should get an employee email', () => {
-    expect(employee.email).toEqual(email)
-})
-
-it('Should have the correct role', () => {
-    expect(employee.getRole()).toEqual("Employee")
-})
 
 
 
